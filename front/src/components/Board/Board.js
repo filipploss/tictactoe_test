be67a7e9bd0913f06b6517ed24acbb5c647a8983
Filipp Loss <filipploss@MacBook-Pro-Filipp.local>
2020-03-16 18:37:25 +0300

    logic

--- front/src/components/Board/Board.js
+++ front/src/components/Board/Board.js
@@ -9,15 +9,6 @@ import CurrentGame from "../CurrentGame";
 
 export default class Board extends Component {
   state = {
-    cell1: "",
-    cell2: "",
-    cell3: "-",
-    cell4: "-",
-    cell5: "-",
-    cell6: "-",
-    cell7: "-",
-    cell8: "-",
-    cell9: "-",
     playerCommand: "",
     aiCommand: "",
     board: [],
@@ -25,6 +16,31 @@ export default class Board extends Component {
     winner: "",
     team: ""
   };
+  // componentDidMount() {
+  //   const fetchMove = async url => {
+  //     const response = await fetch(url);
+  //     const myJson = await response.json();
+  //     console.log("Match Started", JSON.stringify(myJson));
+  //     let playerCommand = await myJson.result.player;
+  //     let aiCommand = await myJson.result.ai;
+  //     let board = await myJson.result.board;
+  //     //  const match = await function() {
+  //        let matchEnd = await myJson.result.end
+  //       console.log('matchEnd= ', matchEnd)
+
+  //     // if (myJson.result.end) {
+  //     //   matchEnd = await myJson.result.end
+  //     //   console.log('matchEnd= ', matchEnd)ç
+  //     // }
+  //     let winner = await myJson.result.winner;
+  //     let team = await myJson.result.team;
+  //     // console.log(playerCommand)
+  //     this.setState({ playerCommand, aiCommand, board});
+  //     // console.log(this.state)
+  //   };
+  //   fetchMove("http://localhost:3001/api/game");
+  // }
+
   componentDidMount() {
     const fetchMove = async url => {
       const response = await fetch(url);
@@ -33,28 +49,62 @@ export default class Board extends Component {
       let playerCommand = await myJson.result.player;
       let aiCommand = await myJson.result.ai;
       let board = await myJson.result.board;
+      //  const match = await function() {
+      // let matchEnd = await myJson.result.end;
+      //   console.log('matchEnd= ', matchEnd)
+
       // if (myJson.result.end) {
-      //   let matchEnd = myJson.result.end
+      //   matchEnd = await myJson.result.end
+      //   console.log('matchEnd= ', matchEnd)ç
       // }
-      let winner = await myJson.result.winner;
-      let team = await myJson.result.team;
+
       // console.log(playerCommand)
-      this.setState({ playerCommand, aiCommand, board, winner, team });
+      this.setState({ playerCommand, aiCommand, board });
       // console.log(this.state)
     };
     fetchMove("http://localhost:3001/api/game");
   }
 
-  matchRestart() {
+  // componentDidUpdate() {
+  //   const fetchMove = async url => {
+  //     const response = await fetch(url);
+  //     const myJson = await response.json();
+  //     console.log("Match Started", JSON.stringify(myJson));
+  //     // let playerCommand = await myJson.result.player;
+  //     // let aiCommand = await myJson.result.ai;
+  //     let board = await myJson.result.board;
+  //     //  const match = await function() {
+  //     let matchEnd = await myJson.result.end || null;
+  //       console.log('matchEnd= ', matchEnd)
+
+  //     // if (myJson.result.end) {
+  //     //   matchEnd = await myJson.result.end
+  //     //   console.log('matchEnd= ', matchEnd)ç
+  //     // }
+  //     let winner = await myJson.result.winner;
+  //     let team = await myJson.result.team;
+  //     // console.log(playerCommand)
+  //     this.setState({winner, team, matchEnd});
+  //     console.log(this.state)
+  //   };
+  //   fetchMove("http://localhost:3001/api/game");
+  // }
+
+  matchRestart = () => {
     const fetchMove = async url => {
       const response = await fetch(url, {
         method: "POST"
       });
       const myJson = await response.json();
+      let board = await myJson.result.board;
       console.log("Match Restart", JSON.stringify(myJson));
+      this.setState({
+        board, matchEnd: null, winner: null
+      });
+      console.log("state!= ", this.state);
     };
     fetchMove("http://localhost:3001/api/game/reset");
-  }
+  };
 
   matchNext() {
     const fetchMove = async url => {
@@ -90,7 +140,17 @@ export default class Board extends Component {
       });
 
       const myJson = await response.json();
+      const board = await myJson.result.board;
       console.log(JSON.stringify(myJson));
+      console.log("Cell", cell);
+      const matchEnd = await myJson.result.end;
+      const winner = await myJson.result.winner;
+      const team = await myJson.result.team;
+      console.log("matchEnd= ", matchEnd);
+      this.setState({ board, matchEnd, winner, team });
+      console.log("State after move", this.state);
+      // let matchEnd = await myJson.result.end;
+      // console.log('matchEnd= ', matchEnd)
       // console.log(this.state);
     };
     fetchMove("http://localhost:3001/api/game/move");
@@ -99,26 +159,46 @@ export default class Board extends Component {
   render() {
     return (
       <div className="main-container">
-      <div className="container ">
-        {this.state.board.map(item =>
-          item.map(item => {
-            return (
-              <div className="column">
-              {console.log(item)}
-              {item}
-              </div>)
-              
-          })
-        )}
-           </div>
-      <div className="results">
-      <CurrentGame />
-      <Score />
-    </div>
-    <button onClick={this.matchRestart}>Restart Match</button>
-    <button onClick={this.matchNext}>Next Match</button>
-    <button onClick={this.gameReset}>Reset All Games</button>
-   
+        <div className="container ">
+          {this.state.board.map((item, i) =>
+            item.map((item, index) => {
+              return (
+                <div
+                  className="column"
+                  key={
+                    i === 1
+                      ? (index = index + 3)
+                      : i === 2
+                      ? (index = index + 6)
+                      : index
+                  }
+                  onClick={() => {
+                    if (typeof item === "number" && !this.state.matchEnd) {
+                      this.makeMove(index + 1);
+                    }
+                    // console.log('clicked cell: ', )
+                  }}
+                >
+                  {/* {console.log('i= ', i)}
+                  {console.log('index= ', index)} */}
+                  {/* {console.log(item)}
+                  {console.log(this.state)} */}
+                  {/* {item} */}
+                  {/* {console.log(index)}
+                  {console.log(indexx)} */}
+                  {item === "X" ? "X" : item === "O" ? "O" : ""}
+                </div>
+              );
+            })
+          )}
+        </div>
+        <div className="results">
+          <CurrentGame />
+          <Score />
+        </div>
+        <button onClick={this.matchRestart}>Restart Match</button>
+        <button onClick={this.matchNext}>Next Match</button>
+        <button onClick={this.gameReset}>Reset All Games</button>
       </div>
       // <Container className="container ">
       //   <div>
@@ -185,7 +265,7 @@ export default class Board extends Component {
       //         onClick={() => this.makeMove(9)}
       //       ></Col>
       //     </Row>
-         
+
       // </Container>
     );
   }
